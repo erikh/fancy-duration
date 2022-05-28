9a6d4c5af2b844e658bb3b24b938b03f46993cbf
Erik Hollensbe <git@hollensbe.org>
2022-05-28 13:33:07 -0700

    to the mooooon
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

---
+++ benches/basic.rs
@@ -0,0 +1,52 @@
+use std::time::Duration as StdDuration;
+use time::Duration as TimeDuration;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use fancy_duration::FancyDuration;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fancy duration std seconds", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(20), 0)))
+    });
+    c.bench_function("fancy duration time seconds", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(20), 0)))
+    });
+
+    c.bench_function("fancy duration std minutes", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(61), 0)))
+    });
+    c.bench_function("fancy duration time minutes", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(61), 0)))
+    });
+
+    c.bench_function("fancy duration std hours", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(2 * 60 * 60 + 61), 0)))
+    });
+    c.bench_function("fancy duration time hours", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(2 * 60 * 60 + 61), 0)))
+    });
+
+    c.bench_function("fancy duration std days", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(2 * 24 * 60 * 60 + 61), 0)))
+    });
+    c.bench_function("fancy duration time days", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(2 * 24 * 60 * 60 + 61), 0)))
+    });
+
+    c.bench_function("fancy duration std weeks", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(2 * 7 * 24 * 60 * 60 + 61), 0)))
+    });
+    c.bench_function("fancy duration time weeks", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(2 * 7 * 24 * 60 * 60 + 61), 0)))
+    });
+
+    c.bench_function("fancy duration std months", |b| {
+        b.iter(|| FancyDuration(StdDuration::new(black_box(31 * 24 * 60 * 60 + 61), 0)))
+    });
+    c.bench_function("fancy duration time months", |b| {
+        b.iter(|| FancyDuration(TimeDuration::new(black_box(31 * 24 * 60 * 60 + 61), 0)))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -52,6 +52,10 @@ where
             return "0".to_string();
         }
 
+        let months = time / 30 / 24 / 60 / 60;
+        time -= months * 30 * 24 * 60 * 60;
+        let weeks = time / 7 / 24 / 60 / 60;
+        time -= weeks * 7 * 24 * 60 * 60;
         let days = time / 24 / 60 / 60;
         time -= days * 24 * 60 * 60;
         let hours = time / 60 / 60;
@@ -59,31 +63,35 @@ where
         let minutes = time / 60;
         time -= minutes * 60;
 
-        format!(
-            "{}{}{}{}",
-            if days >= 1 {
-                format!("{}d ", days)
-            } else {
-                "".to_string()
-            },
-            if hours >= 1 {
-                format!("{}h ", hours)
-            } else {
-                "".to_string()
-            },
-            if minutes >= 1 {
-                format!("{}m ", minutes)
-            } else {
-                "".to_string()
-            },
-            if time >= 1 {
-                format!("{}s", time)
-            } else {
-                "".to_string()
-            },
-        )
-        .trim_end()
-        .to_string()
+        let mut itoa = itoa::Buffer::new();
+
+        let s = if months >= 1 {
+            itoa.format(months).to_string() + "m "
+        } else {
+            "".to_string()
+        } + &(if weeks >= 1 {
+            itoa.format(weeks).to_string() + "w "
+        } else {
+            "".to_string()
+        }) + &(if days >= 1 {
+            itoa.format(days).to_string() + "d "
+        } else {
+            "".to_string()
+        }) + &(if hours >= 1 {
+            itoa.format(hours).to_string() + "h "
+        } else {
+            "".to_string()
+        }) + &(if minutes >= 1 {
+            itoa.format(minutes).to_string() + "m "
+        } else {
+            "".to_string()
+        }) + &(if time >= 1 {
+            itoa.format(time).to_string() + "s"
+        } else {
+            "".to_string()
+        });
+
+        s.trim_end().to_string()
     }
 }
 
@@ -116,6 +124,14 @@ mod tests {
             FancyDuration(Duration::new(24 * 60 * 60 + 324, 0)).to_string(),
             "1d 5m 24s"
         );
+        assert_eq!(
+            FancyDuration(Duration::new(27 * 24 * 60 * 60 + 324, 0)).to_string(),
+            "3w 6d 5m 24s"
+        );
+        assert_eq!(
+            FancyDuration(Duration::new(99 * 24 * 60 * 60 + 324, 0)).to_string(),
+            "3m 1w 2d 5m 24s"
+        );
     }
 
     #[test]
@@ -140,6 +156,14 @@ mod tests {
         assert_eq!(
             FancyDuration(time::Duration::new(24 * 60 * 60 + 324, 0)).to_string(),
             "1d 5m 24s"
+        );
+        assert_eq!(
+            FancyDuration(time::Duration::new(27 * 24 * 60 * 60 + 324, 0)).to_string(),
+            "3w 6d 5m 24s"
+        );
+        assert_eq!(
+            FancyDuration(time::Duration::new(99 * 24 * 60 * 60 + 324, 0)).to_string(),
+            "3m 1w 2d 5m 24s"
         );
     }
 }
