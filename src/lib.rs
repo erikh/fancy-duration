8d355cc6d4987824c0d63898e917376ae695e690
Erik Hollensbe <git@hollensbe.org>
2023-05-10 22:09:49 -0700

    parse durations in the same format
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@
 //!     assert_eq!(FancyDuration(Duration::new(600, 0)).to_string(), "10m");
 //!     assert_eq!(FancyDuration(Duration::new(120, 0)).to_string(), "2m");
 //!     assert_eq!(FancyDuration(Duration::new(185, 0)).to_string(), "3m 5s");
+//!     assert_eq!(FancyDuration::<Duration>::parse("3m 5s").unwrap().duration(), Duration::new(185, 0));
+//!     assert_eq!(FancyDuration(Duration::new(185, 0)).to_string(), "3m 5s");
+//!
+//!     // also works with time::Duration from the `time` crate
+//!     assert_eq!(FancyDuration(time::Duration::new(20, 0)).to_string(), "20s");
+//!     assert_eq!(FancyDuration(time::Duration::new(600, 0)).to_string(), "10m");
+//!     assert_eq!(FancyDuration(time::Duration::new(120, 0)).to_string(), "2m");
+//!     assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
+//!     assert_eq!(FancyDuration::<time::Duration>::parse("3m 5s").unwrap().duration(), time::Duration::new(185, 0));
+//!     assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
 //! }
 //! ```
 //!
@@ -33,7 +43,26 @@ impl AsSecs for time::Duration {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct FancyDuration<D: AsSecs>(pub D);
+
+impl FancyDuration<time::Duration> {
+    pub fn parse(s: &str) -> Result<Self, anyhow::Error> {
+        Ok(FancyDuration::new(time::Duration::new(
+            FancyDuration::<time::Duration>::parse_to_seconds(s)?,
+            0,
+        )))
+    }
+}
+
+impl FancyDuration<Duration> {
+    pub fn parse(s: &str) -> Result<Self, anyhow::Error> {
+        Ok(FancyDuration::new(Duration::new(
+            FancyDuration::<Duration>::parse_to_seconds(s).map(|s| s.abs() as u64)?,
+            0,
+        )))
+    }
+}
 
 impl<D> FancyDuration<D>
 where
@@ -42,6 +71,13 @@ where
     /// Construct a fancier duration!
     pub fn new(d: D) -> Self {
         Self(d)
+    }
+
+    pub fn duration(&self) -> D
+    where
+        D: Clone,
+    {
+        self.0.clone()
     }
 
     /// Show the duration in a fancier format!
@@ -92,6 +128,54 @@ where
         });
 
         s.trim_end().to_string()
+    }
+
+    fn parse_to_seconds(s: &str) -> Result<i64, anyhow::Error> {
+        let mut secs: i64 = 0;
+        let mut past_minutes = false;
+
+        for item in s.split(" ").collect::<Vec<&str>>().iter().rev() {
+            let item = item.trim();
+
+            match item.chars().last() {
+                Some('s') => {
+                    let item = item.strip_suffix('s').unwrap();
+                    let result: i64 = item.parse()?;
+                    secs += result
+                }
+                Some('m') => {
+                    let item = item.strip_suffix('m').unwrap();
+                    let result: i64 = item.parse()?;
+                    secs += if past_minutes {
+                        result * 60 * 60 * 24 * 30
+                    } else {
+                        past_minutes = true;
+                        result * 60
+                    }
+                }
+                Some('h') => {
+                    past_minutes = true;
+                    let item = item.strip_suffix('h').unwrap();
+                    let result: i64 = item.parse()?;
+                    secs += result * 60 * 60;
+                }
+                Some('d') => {
+                    past_minutes = true;
+                    let item = item.strip_suffix('d').unwrap();
+                    let result: i64 = item.parse()?;
+                    secs += result * 60 * 60 * 24;
+                }
+                Some('w') => {
+                    past_minutes = true;
+                    let item = item.strip_suffix('w').unwrap();
+                    let result: i64 = item.parse()?;
+                    secs += result * 60 * 60 * 24 * 7;
+                }
+                Some(_) | None => {}
+            }
+        }
+
+        Ok(secs)
     }
 }
 
@@ -165,5 +249,32 @@ mod tests {
             FancyDuration(time::Duration::new(99 * 24 * 60 * 60 + 324, 0)).to_string(),
             "3m 1w 2d 5m 24s"
         );
+    }
+
+    #[test]
+    fn test_parse_duration() {
+        let duration_table = [
+            ("10s", Duration::new(10, 0)),
+            ("3m 5s", Duration::new(185, 0)),
+            ("3m 2w 2d 10m 10s", Duration::new(9159010, 0)),
+        ];
+
+        let time_table = [
+            ("10s", time::Duration::new(10, 0)),
+            ("3m 5s", time::Duration::new(185, 0)),
+            ("3m 2w 2d 10m 10s", time::Duration::new(9159010, 0)),
+        ];
+
+        for item in duration_table {
+            let fancy = FancyDuration::<Duration>::parse(item.0).unwrap();
+            assert_eq!(fancy.duration(), item.1);
+            assert_eq!(FancyDuration::new(item.1).to_string(), item.0);
+        }
+
+        for item in time_table {
+            let fancy = FancyDuration::<Duration>::parse(item.0).unwrap();
+            assert_eq!(fancy.duration(), item.1);
+            assert_eq!(FancyDuration::new(item.1).to_string(), item.0);
+        }
     }
 }
