0672776265382b36b71e252a660f242217958124
Erik Hollensbe <git@hollensbe.org>
2023-09-23 09:37:43 -0700

    time crate is no longer required; examples for chrono as well
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,27 @@
 //!     assert_eq!(FancyDuration::<Duration>::parse("3m 5s").unwrap().duration(), Duration::new(185, 0));
 //!     assert_eq!(FancyDuration(Duration::new(185, 0)).to_string(), "3m 5s");
 //!
-//!     // also works with time::Duration from the `time` crate
-//!     assert_eq!(FancyDuration(time::Duration::new(20, 0)).to_string(), "20s");
-//!     assert_eq!(FancyDuration(time::Duration::new(600, 0)).to_string(), "10m");
-//!     assert_eq!(FancyDuration(time::Duration::new(120, 0)).to_string(), "2m");
-//!     assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
-//!     assert_eq!(FancyDuration::<time::Duration>::parse("3m 5s").unwrap().duration(), time::Duration::new(185, 0));
-//!     assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
+//!     #[cfg(feature = "time")]
+//!     {
+//!         // also works with time::Duration from the `time` crate
+//!         assert_eq!(FancyDuration(time::Duration::new(20, 0)).to_string(), "20s");
+//!         assert_eq!(FancyDuration(time::Duration::new(600, 0)).to_string(), "10m");
+//!         assert_eq!(FancyDuration(time::Duration::new(120, 0)).to_string(), "2m");
+//!         assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
+//!         assert_eq!(FancyDuration::<time::Duration>::parse("3m 5s").unwrap().duration(), time::Duration::new(185, 0));
+//!         assert_eq!(FancyDuration(time::Duration::new(185, 0)).to_string(), "3m 5s");
+//!     }
+//!
+//!     #[cfg(feature = "chrono")]
+//!     {
+//!         // also works with time::Duration from the `time` crate
+//!         assert_eq!(FancyDuration(chrono::Duration::seconds(20)).to_string(), "20s");
+//!         assert_eq!(FancyDuration(chrono::Duration::seconds(600)).to_string(), "10m");
+//!         assert_eq!(FancyDuration(chrono::Duration::seconds(120)).to_string(), "2m");
+//!         assert_eq!(FancyDuration(chrono::Duration::seconds(185)).to_string(), "3m 5s");
+//!         assert_eq!(FancyDuration::<chrono::Duration>::parse("3m 5s").unwrap().duration(), chrono::Duration::seconds(185));
+//!         assert_eq!(FancyDuration(chrono::Duration::seconds(185)).to_string(), "3m 5s");
+//!     }
 //! }
 //! ```
 //!
